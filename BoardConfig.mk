LineageOS/android_device_motorola_channel

channel: Add board-info.txt
* Fixes flashall in fastbootd

Change-Id: Iae2f33113317cfaadeff7a3610463880077c35e0

BoardConfig.mk
@@ -20,6 +20,7 @@
 DEVICE_PATH := device/motorola/channel
 
 # Assertions
+TARGET_BOARD_INFO_FILE := device/motorola/channel/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := channel
 
 # Init

Evervolv/android_device_moto_channel

BoardConfig.mk
@@ -20,6 +20,7 @@
 DEVICE_PATH := device/moto/channel
 
 # Assertions
+TARGET_BOARD_INFO_FILE := device/moto/channel/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := channel
 
 # Init