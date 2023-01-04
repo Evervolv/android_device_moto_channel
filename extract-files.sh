ae48d7c79457e9a181f5ff26d80971bc4dd802d9
Andrew Hexen <SyberHexen@gmail.com>
2023-01-04 13:40:17 -0800

    channel: kang sensors.ssc.so from motorola chef
    'chef user 10 QPTS30.61-18-16-16 c8402 release-keys'
    
    * Fixes chop chop gesture while maintaining deep sleep.
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Iea762b11a2859294e6d66a60d5124b923e4d769c

--- extract-files.sh
+++ extract-files.sh
@@ -8,12 +8,9 @@
 
 function blob_fixup() {
     case "${1}" in
-        # Use VNDK 32 libutils
-        vendor/lib/hw/audio.primary.sdm632.so | vendor/lib/libmotaudioutils.so | vendor/lib/libsensorndkbridge.so | vendor/lib/libtinycompress_vendor.so)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
-            ;;
-        vendor/lib/soundfx/libspeakerbundle.so | vendor/lib/soundfx/libmmieffectswrapper.so)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+        # libutils-v32
+        vendor/lib/sensors.ssc.so | vendor/lib64/sensors.ssc.so)
+            "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
             ;;
     esac
 }
